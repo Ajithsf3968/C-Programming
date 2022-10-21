a3b04314bf1047faac68e841f308907427c5b8e5
Ajithkumar <ajith.sf3968@gmail.com>
2022-10-21 19:29:10 +0530

    FT-3155 Added searching files into advanced c# concepts

---
+++ OOPSConcepts/Algorithms/Search/BinarySearch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace BinarySearch;
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        int[] arr={10,20,30,40,50};
+        int start=0,end=arr.Length;
+        
+        System.Console.WriteLine("entet the element:");
+        int element=int.Parse(Console.ReadLine());
+        for(int i=0;i<arr.Length;i++)
+        {
+            int mid=(start+end)/2;
+            if(element==arr[mid])
+            {
+                System.Console.WriteLine("element found at:"+mid);
+                break;
+            }       
+            else if(element<arr[mid])
+            {
+            end=mid-1;
+            }
+            else if(element>arr[mid])
+            {
+               start=mid+1;
+            }
+        }
+    }
+}
---
+++ OOPSConcepts/Algorithms/Search/LinearSearch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LinearSearch;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int []arr={1,23,45,5,6};
+        System.Console.WriteLine("Enter the element to find:");
+        int element =int.Parse(Console.ReadLine());
+        for(int i=0;i<arr.Length;i++)
+        {
+            if(arr[i]==element)
+            {
+                System.Console.WriteLine("Element "+" "+element+"is"+"found at "+i);
+            }
+        }
+        System.Console.WriteLine("Element not found");
+        
+    }
+}
